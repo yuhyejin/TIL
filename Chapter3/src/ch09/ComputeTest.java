d1a281672ad2733d25a792c565e1e25774c58df7
hjeu <hj_eu@naver.com>
2023-04-12 22:32:42 +0900

    추상 클래스

---
+++ Chapter3/src/ch09/ComputeTest.java
@@ -0,0 +1,11 @@
+package ch09;
+
+public class ComputeTest {
+
+	public static void main(String[] args) {
+		
+		Computer desktop = new Desktop();
+		
+	}
+
+}
---
+++ Chapter3/src/ch09/NoteBook.java
@@ -0,0 +1,10 @@
+package ch09;
+
+public abstract class NoteBook extends Computer {
+
+	@Override
+	public void display() {
+		System.out.println("NoteBook display");
+	}
+
+}
